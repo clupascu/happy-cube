b6fb86ab12c02b491e24c4fa407a5d362e592dad
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-05-10 23:59:57 +0300

    tested another cube configuration

--- lib/run.rb
+++ lib/run.rb
@@ -3,7 +3,8 @@ $:.unshift File.expand_path('..', __FILE__)
 require 'happy_cube'
 
 # See piece.rb for an explanation of how Pieces are constructed
-pieces = [
+
+yellow_green_cube = [
   HappyCube::Piece.new([0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]),
   HappyCube::Piece.new([0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0]),
   HappyCube::Piece.new([0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0]),
@@ -12,6 +13,14 @@ pieces = [
   HappyCube::Piece.new([0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1])
 ]
 
-solver = HappyCube::Solver.new(pieces)
+pink_cube = [
+  HappyCube::Piece.new([0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]),
+  HappyCube::Piece.new([1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1]),
+  HappyCube::Piece.new([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0]),
+  HappyCube::Piece.new([1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1]),
+  HappyCube::Piece.new([1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1]),
+  HappyCube::Piece.new([0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0])
+]
 
+solver = HappyCube::Solver.new(pink_cube)
 puts solver.solve
