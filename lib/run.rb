042311618654c218b14558ef78f4f771bd9b82ef
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-05-15 22:04:53 +0300

    easier way to construct pieces

--- lib/run.rb
+++ lib/run.rb
@@ -2,25 +2,47 @@ $:.unshift File.expand_path('..', __FILE__)
 
 require 'happy_cube'
 
-# See piece.rb for an explanation of how Pieces are constructed
+# Create a piece by specifying its edge configuration:
+# 16 characters that can be 0s (gaps) or 1s (filled square)
+#
+# These should be specified in clockwise order, starting
+# from the top left corner.
+#
+# For examplethe edge configuration:
+# 
+#  '0101001000101101'
+#
+# corresponds to this piece:
+#
+#  █ █
+# ████
+#  ████
+# ████
+# █ █
+#
+
+def create_piece(edge_configuration)
+  HappyCube::Piece.new edge_configuration.chars.map &:to_i
+end
 
 yellow_green_cube = [
-  HappyCube::Piece.new([0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]),
-  HappyCube::Piece.new([0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0]),
-  HappyCube::Piece.new([0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0]),
-  HappyCube::Piece.new([0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1]),
-  HappyCube::Piece.new([0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]),
-  HappyCube::Piece.new([0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1])
+  create_piece('0010010101010101'),
+  create_piece('0010001011011010'),
+  create_piece('0101001001010010'),
+  create_piece('0010110100101101'),
+  create_piece('0010110110101101'),
+  create_piece('0010001011010101')
 ]
 
 pink_cube = [
-  HappyCube::Piece.new([0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1]),
-  HappyCube::Piece.new([1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1]),
-  HappyCube::Piece.new([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0]),
-  HappyCube::Piece.new([1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1]),
-  HappyCube::Piece.new([1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1]),
-  HappyCube::Piece.new([0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0])
+  create_piece('0001110110101101'),
+  create_piece('1010010111011101'),
+  create_piece('0101010101010010'),
+  create_piece('1010001000100011'),
+  create_piece('1010001001010001'),
+  create_piece('0110010100100010')
 ]
 
-solver = HappyCube::Solver.new(pink_cube)
+
+solver = HappyCube::Solver.new(yellow_green_cube)
 puts solver.solve
