2b0f9d9c10c061fa085f9accb7c21609e6647d05
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-05-15 22:51:53 +0300

    added more cubes

--- lib/run.rb
+++ lib/run.rb
@@ -43,6 +43,41 @@ pink_cube = [
   create_piece('0110010100100010')
 ]
 
+orange_cube = [
+  create_piece('0101001011011010'),
+  create_piece('0101001001010010'),
+  create_piece('0010110100101101'),
+  create_piece('0010001011010010'),
+  create_piece('1101110100100010'),
+  create_piece('1010010101010101'),
+]
 
-solver = HappyCube::Solver.new(yellow_green_cube)
+green_marble_cube = [
+  create_piece('0101001011011010'),
+  create_piece('0010001000100010'),
+  create_piece('0101001011010010'),
+  create_piece('0101010101011010'),
+  create_piece('0101101001011101'),
+  create_piece('1010001001011101'),
+]
+
+blue_marble_cube = [
+  create_piece('0011001001010101'),
+  create_piece('0011101001100010'),
+  create_piece('0010010101010101'),
+  create_piece('1101100110100001'),
+  create_piece('0100011000100010'),
+  create_piece('1010110011011101'),
+]
+
+violet_marble_cube = [
+  create_piece('0010110011010101'),
+  create_piece('0011000110100011'),
+  create_piece('0110001011100010'),
+  create_piece('0001111001100101'),
+  create_piece('1100010101010001'),
+  create_piece('1010110001100001'),
+]
+
+solver = HappyCube::Solver.new(violet_marble_cube)
 puts solver.solve
